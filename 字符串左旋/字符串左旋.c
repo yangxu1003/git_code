020688b4922d640e7a381163c049c9a097a35c86
yangxu1003 <1773349271@qq.com>
2020-11-17 21:12:48 +0800

    字符串左旋

---
+++ 字符串左旋/字符串左旋.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#pragma warning (disable:4996)
+#include <string.h>
+void LeftRotate(char str[], char D[], int len, int K)
+{
+    K %= len;
+    char* d = D;
+    char* s = str;
+    while (*d)
+    {
+        d++;
+    }
+    while (*s)
+    {
+        *d++ = *s++;
+    }
+    while (len--)
+    {
+        printf("%c", *(D + (K++)));
+    }
+    printf("\n");
+}
+int main()
+{
+    char str[100] = { 0 };
+    printf("ÊäÈë×Ö·û´®£º");
+    fgets(str, sizeof(str) - 1, stdin);
+    str[strlen(str) - 1] = '\0';
+    char data[64];
+    int k = 0;
+    int size = strlen(str);
+    strcpy(data, str);
+    printf("Ïò×óÐý×ªk¸ö×Ö·û:");
+    scanf("%d", &k);
+    LeftRotate(str, data, size, k);
+    return 0;
+}
